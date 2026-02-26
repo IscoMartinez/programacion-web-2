d94c7c324b2f4064e88894b976945b9b2a068145
IscoMartinez <iscomtz1128@gmail.com>
2026-02-25 22:57:31 -0600

    inicio de programación en javascript

---
+++ inicio_de_programacion_en_javascript/app1.js
@@ -0,0 +1,10 @@
+
+var nombre = prompt("Ingresa tu nombre: ");
+var edad = prompt("Ingresa tu edad: ");
+var carrera = prompt("Ingresa tu carrera: ");
+var correo = prompt("Ingresa tu correo electrónico: ");
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Carrera: " + carrera);
+console.log("Correo electrónico: " + correo);
---
+++ inicio_de_programacion_en_javascript/app2.js
@@ -0,0 +1,15 @@
+var num1 = 38281;
+var num2 = 589330;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multiplicacion = num1 * num2;
+var division = num1 / num2;
+var modulo = num1 % num2;
+
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+console.log("Módulo: " + modulo);

cb39af26ac4e2c561e0de97196aa176850ce4f55
IscoMartinez <iscomtz1128@gmail.com>
2026-02-27 23:22:52 -0600

    estructura de control if

---
+++ estructura_de_control_if/app.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt("Ingresa el primer numero num1: "));
+var num2 = parseInt(prompt("Ingresa el segundo numero num2: "));
+
+if (num1 > num2) {
+    console.log("num1 es mayor");
+} else if (num1 < num2) {
+    console.log("num2 es mayor");
+} else {
+    console.log("Los numeros son iguales");
+}
